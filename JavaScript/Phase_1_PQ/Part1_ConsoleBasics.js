5e9b37c5b8fcf9c286513cf68d0c9cf519801447
Kirtan Gupta <kgsk2594@gmail.com>
2026-07-13 22:45:04 +0530

    Console & Basics and

---
+++ JavaScript/Phase_1_PQ/Part1_ConsoleBasics.js
@@ -0,0 +1,15 @@
+/*
+## Console & Basics
+
+1. Print `"Hello JavaScript"` in the console.
+2. Print your name, age, and city using one `console.log()`.
+3. Print a warning message using `console.warn()`.
+4. Print an error message using `console.error()`.
+5. Use `console.table()` to display an array of 5 numbers.
+*/
+
+console.log("Hello JavaScript");
+console.log("John Doe", 25, "New York");
+console.warn("This is a warning message");
+console.error("This is an error message");
+console.table([1, 2, 3, 4, 5]);
---
+++ JavaScript/Phase_1_PQ/Part2_Variables.js
@@ -0,0 +1,40 @@
+
+// ## Variables
+
+// 1. Create a variable called `studentName` and store your name in it.
+var studentName="Kirtan Gupta"
+// 2. Create a variable `age` and print it.
+var age=22
+console.log(age)
+// 3. Create two variables and swap their values.
+var a=10
+var b=20
+console.log("Value of A Before Swapping ",a)
+console.log("Value of A Before Swapping ",a)
+
+c=a
+a=b
+b=c
+console.log("=========================")
+console.log("Value of A After Swapping ",a)
+console.log("Value of A After Swapping ",a)
+
+// 4. Create a constant variable for `PI` and print it.
+const PI=3.14
+console.log("Value of constant PI is ",PI)
+// 5. Declare a variable without assigning a value and print it.
+var x
+console.log(x)
+// 6. Create a variable `score` and increase it by 10.
+var score=0
+console.log(score + 10)
+// 7. Create three variables for first name, last name, and full name.
+
+var firstName="Kirtan"
+var lastName="Gupta"
+var fullName=firstName+lastName
+
+console.log("Full name : ",fullName)
+
+
+
